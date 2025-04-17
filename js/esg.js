8a817d279eecef2e68f8571c01de3b08e6556d59
Long Huizhi <huizhi.l@u.nus.edu>
2025-04-17 17:10:17 +0800

    Update esg.js

--- js/esg.js
+++ js/esg.js
@@ -1,6 +1,11 @@
 function updateESGCharts() {
-  const company = document.getElementById("companySelect").value;
+  const companySelect = document.getElementById("companySelect");
+  const company = companySelect.value;
+  const companyName = companySelect.options[companySelect.selectedIndex].text;
+
   document.getElementById("barChart").src = `charts/${company}_bar.html`;
   document.getElementById("lineChart").src = `charts/${company}_line.html`;
   document.getElementById("heatmapChart").src = `charts/${company}_heatmap.html`;
+
+  document.getElementById("selectedCompanyText").textContent = `You selected: ${companyName}`;
 }
